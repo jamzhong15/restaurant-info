59d9294007019c623e0ea7a00dc5946f0f40d40d
jamzhong15 <jamzhong@gmail.com>
2025-04-01 20:26:56 +0100

    Initial Commit

---
+++ RestaurantInfo/RestaurantInfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantInfoApp.swift
+//  RestaurantInfo
+//
+//  Created by James Zhong on 01/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantInfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RestaurantInfoTests/RestaurantInfoTests.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantInfoTests.swift
+//  RestaurantInfoTests
+//
+//  Created by James Zhong on 01/04/2025.
+//
+
+import Testing
+@testable import RestaurantInfo
+
+struct RestaurantInfoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
