32943dd8319a0a2659cb4e4e4f5d07407fd7e0fd
jamzhong15 <jamzhong@gmail.com>
2025-04-01 20:32:18 +0100

    feat: initialise swiftUI project and setup folder structure

---
+++ RestaurantInfo/ViewModels/RestaurantViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  RestaurantViewModel.swift
+//  RestaurantInfo
+//
+//  Created by James Zhong on 01/04/2025.
+//
+
+import Foundation
---
+++ RestaurantInfo/Views/RestaurantRowView.swift
@@ -0,0 +1,18 @@
+//
+//  RestaurantRowView.swift
+//  RestaurantInfo
+//
+//  Created by James Zhong on 01/04/2025.
+//
+
+import SwiftUI
+
+struct RestaurantRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RestaurantRowView()
+}
